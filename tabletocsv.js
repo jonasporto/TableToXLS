8b7576ed8e9b53f313a4593f62c1f1a5a4bc9d14
jonasporto <jporto.contato@hotmail.com>
2014-12-10 10:18:56 -0200

    improving elements target

--- tabletocsv.js
+++ tabletocsv.js
@@ -1,25 +1,28 @@
-var TableToCSV = {
+
+var TableToXLS = {
     
-        tableClass : '.relatorio',
+        table : 'table',
 
         tr : function(){
-            return document.querySelectorAll( this.tableClass + ' tr ');
+            return document.querySelectorAll( this.table + ' tr ');
         },
 
         getDataInTable : function(){
             
             var i = 0,  data = this.tr();
 
+            this.result = '';
+
             for (; i < data.length; i++){
                 
                 var j = 0, header = data[i].querySelectorAll('th'),
                 body = data[i].querySelectorAll('td'), data_row = '';
 
-                if (header.length > 0 && i === 0) data_row = header;
+                if (header.length > 0) data_row = header;
                 if (body.length > 0) data_row = body;
 
                 for( ; j < data_row.length; j++){
-                    this.result += "\"" + data_row[j].textContent + "\"";
+                    this.result += "\"" + data_row[j].textContent.replace(/(\r\n|\n|\r)/gm,"") + "\"";
                     if (data_row.length -1 != j) this.result += ",";
                     else  this.result += "\n";
                 }
@@ -33,12 +36,10 @@ var TableToCSV = {
              return true;
         },
 
-        export : function(tableClass){
-            if(tableClass) this.tableClass = tableClass;
+        export : function(table){
+            if(table) this.table = table;
             return this.sendToOpenOrDownload(this.getDataInTable());
         },
         
         result : ''
 };
-
-
